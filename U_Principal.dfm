49288fd514c37dd144d5cb696f11b076c8e06926
Reinaldo Silveira <reinaldopsilveira@gmail.com>
2019-09-06 18:14:59 -0300

    Primeiro commit (iníciod o projeto e form principal)

---
+++ U_Principal.dfm
@@ -0,0 +1,16 @@
+object F_Principal: TF_Principal
+  Left = 0
+  Top = 0
+  Caption = 'Exemplo de CRUD (Orientado a Objetos) - Oracle'
+  ClientHeight = 271
+  ClientWidth = 643
+  Color = clBtnFace
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -11
+  Font.Name = 'Tahoma'
+  Font.Style = []
+  OldCreateOrder = False
+  PixelsPerInch = 96
+  TextHeight = 13
+end
---
+++ U_Principal.pas
@@ -0,0 +1,24 @@
+unit U_Principal;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TF_Principal = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  F_Principal: TF_Principal;
+
+implementation
+
+{$R *.dfm}
+
+end.
